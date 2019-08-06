92f9666edee56abee0fc88f32967b44ec4d5a266
dgs65 <52545571+dgs65@users.noreply.github.com>
2019-08-05 20:43:53 -0400

    Improved character swap
    'Q' becomes current Object
    'E' cycles through a list of prefabs

--- CS345-group-project/Assets/Scripts/CharacterSwap.cs
+++ CS345-group-project/Assets/Scripts/CharacterSwap.cs
@@ -5,16 +5,15 @@ using UnityEngine;
 public class CharacterSwap : MonoBehaviour
 {
     private bool _swapped;
+    private int _index = 0;
+    private GameObject _curObject;
 
     public GameObject _FPSController;
-    public GameObject _cylinder;
-    public GameObject _cube;
+    public List<GameObject> _objects;
     // Start is called before the first frame update
     void Start()
     {
-        _FPSController.SetActive(true);
-        _cylinder.SetActive(false);
-        _cube.SetActive(false);
+        _curObject = null;
         _swapped = false;
     }
 
@@ -25,38 +24,30 @@ public class CharacterSwap : MonoBehaviour
         {
             if (_swapped)
             {
-                swapObjects(_cylinder, _FPSController);
+                Destroy(_curObject);
+                _FPSController.SetActive(true);
                 _swapped = false;
             }
             else
             {
-                _cylinder.transform.position = new Vector3(_FPSController.transform.position.x,_FPSController.transform.position.y - .5f,_FPSController.transform.position.z);
-                swapObjects(_FPSController, _cylinder);
+                _curObject = (GameObject) Instantiate(_objects[_index], new Vector3(_FPSController.transform.position.x,_FPSController.transform.position.y - .5f,_FPSController.transform.position.z), Quaternion.identity);
+                _FPSController.SetActive(false);
                 _swapped = true;
                 
             }
         }
         if (Input.GetKeyDown(KeyCode.E))
         {
-            if (_swapped)
+            if(!_swapped)
             {
-                swapObjects(_cube, _FPSController);
-                _swapped = false;
-            }
-            else
-            {
-                _cube.transform.position = new Vector3(_FPSController.transform.position.x, _FPSController.transform.position.y - .5f, _FPSController.transform.position.z);
-                swapObjects(_FPSController, _cube);
-                _swapped = true;
-
+                _index++;
+                if(_index >= _objects.Count)
+                {
+                    _index = 0;
+                }
+                _curObject = _objects[_index];
             }
         }
-    }
-
-    void swapObjects(GameObject curObject, GameObject nextObject)
-    {
-        curObject.SetActive(false);
-        nextObject.SetActive(true);
     }
 
 }
